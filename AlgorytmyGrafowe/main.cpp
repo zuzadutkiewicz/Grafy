986bb75ad5ec66cb43d21251df07ac2d5d7f1ba1
Wlodzimierz Dutkiewicz <wlodzimierz.dutkiewicz@bzwbk.pl>
2015-05-05 16:12:28 +0200

    Dodanie obslugi listy nastepnikow

--- AlgorytmyGrafowe/main.cpp
+++ AlgorytmyGrafowe/main.cpp
@@ -4,12 +4,13 @@ using namespace std;
 
 int main()
 {
-    GenerowanieGrafu generGraf = GenerowanieGrafu(5000);
+    GenerowanieGrafu generGraf = GenerowanieGrafu(10000);
     // generGraf.drukujMacierzSas();
     cout << "Poczatek generowania grafu" << endl;
     generGraf.generujGraf();
     cout << "Koniec generowania grafu " << endl;
     // generGraf.drukujMacierzSasJedynki();
+    // generGraf.drukujListaNast();
 
     return 0;
 }
